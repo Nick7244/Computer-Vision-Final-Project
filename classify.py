0e6a8d74dab11fcc0f69f6ea56e8350f456680d6
Akaash Sanyal <asanyal5@jhu.edu>
2019-11-30 19:23:56 -0500

    Ready to train

--- classify.py
+++ classify.py
@@ -20,28 +20,16 @@ from keras.applications import VGG16
 from keras.applications import imagenet_utils
 from keras.applications.inception_v3 import preprocess_input
 from keras.preprocessing.image import ImageDataGenerator
-from keras.models import Sequential
-from keras.layers import Dropout, Flatten, Dense
+from keras.models import Model, Sequential
+from keras.layers import Dropout, Flatten, Dense, GlobalAveragePooling2D
 import numpy as np
 import argparse
 import pickle
 
-# Variables
-num_class = 43
-train_batch_size = 32
-valid_batch_size = 1
-train_data_dir = 'GTSRB/Training'
-validation_data_dir = 'GTSRB/Validation'
-testing_data_dir = ''
-num_train = 21312
-num_validation = 5328
-num_epochs = 50
-model_file = 'first_try.h5'
-
 # Parse arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-model", "--model", type=str, default="vgg16",
-	help="name of pre-trained network to use")
+        help="name of pre-trained network to use")
 args = vars(ap.parse_args())
 
 # Dictionary of pretrained architectures
@@ -49,7 +37,19 @@ MODELS = {"vgg16": VGG16, "inception": InceptionV3, "resnet": ResNet50}
 
 # Makes sure using a valid model
 if args["model"] not in MODELS.keys():
-	raise AssertionError("Invalid model")
+        raise AssertionError("Invalid model")
+
+# Variables
+num_class = 43
+train_batch_size = 48
+valid_batch_size = 48
+train_data_dir = 'GTSRB/Training'
+validation_data_dir = 'GTSRB/Validation'
+testing_data_dir = ''
+num_train = 21312
+num_validation = 5328
+num_epochs = 25
+model_file = 'first_try_'+args["model"]+'.h5'
 
 # Gets proper shape and preprocessing function
 input_shape = (224,224)
@@ -93,15 +93,28 @@ validation_generator = validation_datagen.flow_from_directory(
 
 # Initializes model with ImageNet weights and prevent retraining
 model_name = MODELS[args["model"]]
-model = model_name(include_top=False, weights="imagenet")
-for layer in model.layers:
+base_model = model_name(include_top=False, weights="imagenet")
+for layer in base_model.layers:
     layer.trainable = False
 
+print('Model loaded.')
+
 # New classification
+x = base_model.output
+x = GlobalAveragePooling2D()(x)
+#x = Flatten(input_shape = base_model.output_shape[1:])(x)
+x = Dense(1024, activation='relu')(x)
+x = Dropout(0.5)(x)
+predictions = Dense(num_class, activation='softmax')(x)
+
+model = Model(inputs=base_model.input, outputs=predictions)
+
+'''
 model.add(Flatten()) 
 model.add(Dense(1024, activation='relu')) 
 model.add(Dropout(0.5)) 
 model.add(Dense(num_class, activation='softmax')) 
+'''
 
 model.compile(loss='categorical_crossentropy', 
               optimizer='rmsprop', 
